874127ec020c836b25486b3782ce173a077927b4
Łukasz Ogrodowczyk <lukasz3090@gmail.com>
2022-04-04 20:58:56 +0200

    1.8

---
+++ zadanie_1.8.py
@@ -0,0 +1,10 @@
+
+'''
+1 rok psi to 5 lat ludzkich
+'''
+
+
+imie = str(input('Podaj imię psa: '))
+wiek = int(input('Podaj wiek psa: '))
+wiek_l = wiek * 5
+print(f'Gdyby {imie} był człowiekiem, miałby {wiek_l} lat.')
